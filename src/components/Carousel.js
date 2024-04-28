33ba65964a284b7b6e96ee07f5046cc9f8afa884
Krishnasai45 <yamanikrishnasai45@gmail.com>
2024-04-28 12:28:49 +0530

    final commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -22,43 +22,5 @@ const Carousel = ({ images }) => {
 
 export default Carousel;
 
-// import React, { useState } from 'react';
-// // import './Carousel.css'; // Import the CSS file
-
-// const Carousel = ({ images }) => {
-//   const [currentIndex, setCurrentIndex] = useState(0);
-
-//   const goToNextSlide = () => {
-//     setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-//   };
-
-//   const goToPrevSlide = () => {
-//     setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-//   };
-
-//   return (
-//     <div className="carousel-container">
-//       <div className="carousel">
-//         {images.map((image, index) => (
-//           <img
-//             key={index}
-//             src={image.image}
-//             alt={`Slide ${index}`}
-//             className="carousel-image"
-//             style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-//           />
-//         ))}
-//       </div>
-//       <button className="carousel-button left" onClick={goToPrevSlide}>
-//         &larr;
-//       </button>
-//       <button className="carousel-button right" onClick={goToNextSlide}>
-//         &rarr;
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Carousel;
 
 
--- src/components/Lands.js
+++ src/components/Lands.js
@@ -2,63 +2,47 @@ import React, { useEffect, useState } from 'react'
 import Card from './Card';
 
 const Lands = () => {
-    const [cards, setCards] = useState([]);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [loading, setLoading] = useState(false);
-    console.log("cardsData",cards)
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        setLoading(true);
-        const response = await fetch(`https://prod-be.1acre.in/lands/?ordering=-updated_at&page=${pageNumber}&page_size=10`);
-        const data = await response.json();
-        setCards((prevCards) => [...prevCards, ...data?.results]);
-        setLoading(false);
-      };
-      fetchData();
-  
-      window.addEventListener('scroll', handleScroll);
-  
+  const [cards, setCards] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(false);
+  console.log("cardsData", cards)
 
-      return () => window.removeEventListener('scroll', handleScroll);
-    }, [pageNumber]);
-  
-    const handleScroll = () => {
-        if (
-            window.innerHeight + document.documentElement.scrollTop +1 >=
-            document.documentElement.scrollHeight
-        ) {
-          setPageNumber((prevPageNumber) => prevPageNumber + 1);
-        }
-      };
-      
-    // "state_name": "Telangana",
-    // "district_name": "Kamareddy",
-    // "mandal_name": "Lingampet",
-    // "village_name": "Jaldipalle",
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await fetch(`https://prod-be.1acre.in/lands/?ordering=-updated_at&page=${pageNumber}&page_size=10`);
+      const data = await response.json();
+      setCards((prevCards) => [...prevCards, ...data?.results]);
+      setLoading(false);
+    };
+    fetchData();
 
-    // "total_land_size_in_acres": {
-    //     "acres": 1.0,
-    //     "guntas": 4.0
-    // },
+    window.addEventListener('scroll', handleScroll);
 
-    // "price_per_acre_crore": {
-    //     "crore": 0.0,
-    //     "lakh": 40.0
-    // },
 
-    // "total_price": 44.0,
-    return (
-      <div>
-        <h1>1Acre Lands for Sale</h1>
-        <div className="main-container">
-          {cards.map((card, index) => (
-            <Card key={index} card={card}/>
-          ))}
-        </div>
-        {loading && <p>Loading...</p>}
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [pageNumber]);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      setPageNumber((prevPageNumber) => prevPageNumber + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>1Acre Lands for Sale</h1>
+      <div className="main-container">
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
       </div>
-    );
+      {loading && <p>Loading...</p>}
+    </div>
+  );
 }
 
 export default Lands
